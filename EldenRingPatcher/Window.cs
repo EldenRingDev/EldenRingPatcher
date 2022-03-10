4937c930d7b04a2ec409ce94483271d400226725
Bia10 <t.locke1234@gmail.com>
2022-03-10 16:43:04 +0100

    refactor

--- EldenRingPatcher/Window.cs
+++ EldenRingPatcher/Window.cs
@@ -7,47 +7,39 @@ namespace EldenRingPatcher
 {
     public class Window
     {
-        private const int WindowTitleMaxLength = 50; // Maximum lenght of window title before its truncated
+        private const int WindowTitleMaxLength = 50; // Maximum length of window title before its truncated
         private static bool verboseOutput = false;
 
         public static List<IntPtr> GetAllHandles(bool outputWindowNames = true)
         {
-            Process[] processList;
             List<IntPtr> windowHandles = new();
-            int indexCounter;
 
-            // Print out (almost) every window title and save their handle
-            processList = Process.GetProcesses();
-            indexCounter = 1;
+            var processList = Process.GetProcesses();
+            var indexCounter = 1;
 
-            if (windowHandles == null) 
-                windowHandles = new List<IntPtr>();
-            else 
-                windowHandles.Clear();
+            windowHandles.Clear();
 
-            foreach (Process process in processList)
+            foreach (var process in processList)
             {
-                if (verboseOutput)
+                if (string.IsNullOrEmpty(process.MainWindowTitle)) continue;              
+                
+                if (verboseOutput) 
                     Console.WriteLine($"{process.ProcessName}: {process.MainWindowHandle}|{process.MainWindowTitle}");
 
-                if (!string.IsNullOrEmpty(process.MainWindowTitle))
+                if (outputWindowNames)
                 {
-                    if (outputWindowNames)
-                    {
-                        string windowTitle = RemoveSpecialCharacters(process.MainWindowTitle);
-                        Console.WriteLine("({0:d}) : {1:s}", indexCounter,
-                            windowTitle.Substring(0, Math.Min(windowTitle.Length, WindowTitleMaxLength)));
-                    }
-
-                    windowHandles.Add(process.MainWindowHandle);
-                    indexCounter++;
+                    var windowTitle = RemoveSpecialCharacters(process.MainWindowTitle);
+                    Console.WriteLine("({0:d}) : {1:s}", indexCounter, windowTitle[..Math.Min(windowTitle.Length, WindowTitleMaxLength)]);
                 }
+
+                windowHandles.Add(process.MainWindowHandle);
+                indexCounter++;
             }
 
             return windowHandles;
         }
 
-        public static string RemoveSpecialCharacters(string str)
+        private static string RemoveSpecialCharacters(string str)
         {
             return Regex.Replace(str, "[^a-zA-Z0-9_. -]+", string.Empty, RegexOptions.Compiled);
         }
